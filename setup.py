d42543ea5f437ba50fd29773ea6d8f9ece1ee1c3
Christoph Muche <christoph@muche.info>
2023-04-10 22:56:41 +0200

    lic

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ plugin_author_email = "dev@cmuche.de"
 plugin_url = "https://github.com/cmuche/octoprint-timelapseplus"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
-plugin_license = "AGPLv3"
+plugin_license = "CC BY-ND"
 
 # Any additional requirements besides OctoPrint should be listed here
 plugin_requires = ["pillow >=6.2.0<7.0.0"]
