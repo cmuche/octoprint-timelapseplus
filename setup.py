be7fdd6f478c3cdd38132c5b04761acc3c2856e6
Christoph Muche <christoph@muche.info>
2023-04-10 20:51:37 +0200

    fix setup package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 plugin_identifier = "timelapseplus"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "timelapseplus"
+plugin_package = "octoprint_timelapseplus"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
