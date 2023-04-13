3d12fd81d8cfa841fd0291718af01d2e1d301cfc
Christoph Muche <christoph@muche.info>
2023-04-13 23:00:53 +0200

    zip store

--- octoprint_timelapseplus/printJob.py
+++ octoprint_timelapseplus/printJob.py
@@ -64,7 +64,7 @@ class PrintJob:
         with zipfile.ZipFile(zipFileName, 'w') as zipMe:
             for file in finishedFiles:
                 baseName = os.path.basename(file)
-                zipMe.write(file, baseName, compress_type=zipfile.ZIP_DEFLATED)
+                zipMe.write(file, baseName, compress_type=zipfile.ZIP_STORED)
 
         shutil.rmtree(self.FOLDER)
         return zipFileName
