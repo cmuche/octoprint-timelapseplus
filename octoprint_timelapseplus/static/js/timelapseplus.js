910b32b63e9be484642b0269659c571975a679df
Christoph Muche <christoph@muche.info>
2023-04-13 20:56:40 +0200

    file list styling

--- octoprint_timelapseplus/static/js/timelapseplus.js
+++ octoprint_timelapseplus/static/js/timelapseplus.js
@@ -12,6 +12,36 @@ $(function() {
         self.videos = ko.observable([]);
         self.renderJobs = ko.observable([]);
 
+        // https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
+        self.humanFileSize = function(size) {
+            let i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+            return (size / Math.pow(1024, i)).toFixed(2) * 1 + " " + ["B", "kB", "MB", "GB", "TB"][i];
+        };
+
+        // https://stackoverflow.com/questions/6108819/javascript-timestamp-to-relative-time
+        self.timeDifference = function(ts) {
+            const msPerMinute = 60 * 1000;
+            const msPerHour = msPerMinute * 60;
+            const msPerDay = msPerHour * 24;
+            const msPerMonth = msPerDay * 30;
+            const msPerYear = msPerDay * 365;
+
+            const elapsed = new Date() - ts * 1000;
+
+            if (elapsed < msPerMinute)
+                return Math.round(elapsed / 1000) + " seconds ago";
+            else if (elapsed < msPerHour)
+                return Math.round(elapsed / msPerMinute) + " minutes ago";
+            else if (elapsed < msPerDay)
+                return Math.round(elapsed / msPerHour) + " hours ago";
+            else if (elapsed < msPerMonth)
+                return Math.round(elapsed / msPerDay) + " days ago";
+            else if (elapsed < msPerYear)
+                return Math.round(elapsed / msPerMonth) + " months ago";
+            else
+                return Math.round(elapsed / msPerYear) + " years ago";
+        };
+
         self.openBlurMask = function(preset) {
             console.log("openBlurMask");
             $("<input type=\"file\">").on("change", function() {
