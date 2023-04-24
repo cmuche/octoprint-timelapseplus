ada59ff628b8b8085d441156098018aad2479ae9
Christoph Muche <christoph@muche.info>
2023-04-24 18:11:32 +0200

    setup description

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = "Better Timelapses"
+plugin_description = "Timelapse+ is a powerful yet lightweight plugin to capture, enhance and render your print timelapses."
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Christoph Muche"
