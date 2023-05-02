140d8094e38c0d38c0ab05443f645755acadd527
Christoph Muche <christoph@muche.info>
2023-05-02 23:22:08 +0200

    multiple qualities

--- octoprint_timelapseplus/helpers/formatHelper.py
+++ octoprint_timelapseplus/helpers/formatHelper.py
@@ -5,11 +5,20 @@ class FormatHelper:
     @staticmethod
     def getVideoFormats():
         return [
-            Format('mp4-h254', 'MP4', None, 'mp4', 'video/mp4', 'H264', 'libxh264'),
-            Format('mp4-h265', 'MP4', None, 'mp4', 'video/mp4', 'H265', 'libx265'),
+            Format('mp4-h264-lq', 'MP4', 'Low Quality', 'mp4', 'video/mp4', 'H264', 'libx264', {'-crf': '28', '-preset': 'medium'}),
+            Format('mp4-h264-nq', 'MP4', 'Normal Quality', 'mp4', 'video/mp4', 'H264', 'libx264', {'-crf': '23', '-preset': 'slow'}),
+            Format('mp4-h264-hq', 'MP4', 'High Quality', 'mp4', 'video/mp4', 'H264', 'libx264', {'-crf': '18', '-preset': 'slower'}),
+
+            Format('mp4-h265-lq', 'MP4', 'Low Quality', 'mp4', 'video/mp4', 'H265', 'libx265', {'-crf': '28', '-preset': 'medium'}),
+            Format('mp4-h265-nq', 'MP4', 'Normal Quality', 'mp4', 'video/mp4', 'H265', 'libx265', {'-crf': '23', '-preset': 'slow'}),
+            Format('mp4-h265-hq', 'MP4', 'High Quality', 'mp4', 'video/mp4', 'H265', 'libx265', {'-crf': '18', '-preset': 'slower'}),
+
             Format('webm', 'WEBM', None, 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '15', '-b:v': '0'}),
+
             Format('avi-mpeg4', 'AVI', None, 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4'),
+
             Format('mpeg2', 'MPEG', None, 'mpeg', 'video/mpeg', 'MPEG-2', 'mpeg2video'),
+
             Format('gif', 'GIF', None, 'gif', 'image/gif', None, None)
         ]
 
