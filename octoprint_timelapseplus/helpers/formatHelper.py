a886da6da7ef573eab83f096040d370a543b3209
Christoph Muche <christoph@muche.info>
2023-05-03 22:02:07 +0200

    refine format presets

--- octoprint_timelapseplus/helpers/formatHelper.py
+++ octoprint_timelapseplus/helpers/formatHelper.py
@@ -21,14 +21,19 @@ class FormatHelper:
             Format('mp4-h265-hq', 'MP4', 'High Quality', 'mp4', 'video/mp4', 'H.265', 'libx265', {'-crf': '25', '-preset': 'slow'}),
             Format('mp4-h265-uq', 'MP4', 'Ultra Quality', 'mp4', 'video/mp4', 'H.265', 'libx265', {'-crf': '20', '-preset': 'slower'}),
 
-            Format('webm-vp9-prevq', 'WEBM', 'Draft Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '50', '-b:v': '0'}),
-            Format('webm-vp9-lq', 'WEBM', 'Low Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '40', '-b:v': '0'}),
-            Format('webm-vp9-nq', 'WEBM', 'Normal Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '30', '-b:v': '0'}),
-            Format('webm-vp9-hq', 'WEBM', 'High Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '20', '-b:v': '0'}),
+            Format('webm-vp9-prevq', 'WEBM', 'Draft Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '60', '-b:v': '0'}),
+            Format('webm-vp9-lq', 'WEBM', 'Low Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '50', '-b:v': '0'}),
+            Format('webm-vp9-nq', 'WEBM', 'Normal Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '40', '-b:v': '0'}),
+            Format('webm-vp9-hq', 'WEBM', 'High Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '30', '-b:v': '0'}),
+            Format('webm-vp9-uq', 'WEBM', 'Ultra Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '20', '-b:v': '0'}),
 
-            Format('avi-mpeg4', 'AVI', None, 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '10'}),
+            Format('avi-mpeg4-lq', 'AVI', 'Low Quality', 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '22'}),
+            Format('avi-mpeg4-nq', 'AVI', 'Normal Quality', 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '10'}),
+            Format('avi-mpeg4-hq', 'AVI', 'High Quality', 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '2'}),
 
-            Format('mpeg2', 'MPEG', None, 'mpg', 'video/mpeg', 'MPEG-2', 'mpeg2video'),
+            Format('mpeg2-lq', 'MPEG', 'Low Quality', 'mpg', 'video/mpeg', 'MPEG-2', 'mpeg2video', {'-qscale:v': '22'}),
+            Format('mpeg2-nq', 'MPEG', 'Normal Quality', 'mpg', 'video/mpeg', 'MPEG-2', 'mpeg2video', {'-qscale:v': '10'}),
+            Format('mpeg2-hq', 'MPEG', 'High Quality', 'mpg', 'video/mpeg', 'MPEG-2', 'mpeg2video', {'-qscale:v': '2'}),
 
             Format('gif', 'GIF', None, 'gif', 'image/gif', None, None)
         ]
