a0f8fdaa65db5173da30410259f9489d956ce726
Christoph Muche <christoph@muche.info>
2023-05-03 22:06:45 +0200

    +vp8

--- octoprint_timelapseplus/helpers/formatHelper.py
+++ octoprint_timelapseplus/helpers/formatHelper.py
@@ -21,11 +21,15 @@ class FormatHelper:
             Format('mp4-h265-hq', 'MP4', 'High Quality', 'mp4', 'video/mp4', 'H.265', 'libx265', {'-crf': '25', '-preset': 'slow'}),
             Format('mp4-h265-uq', 'MP4', 'Ultra Quality', 'mp4', 'video/mp4', 'H.265', 'libx265', {'-crf': '20', '-preset': 'slower'}),
 
-            Format('webm-vp9-prevq', 'WEBM', 'Draft Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '60', '-b:v': '0'}),
-            Format('webm-vp9-lq', 'WEBM', 'Low Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '50', '-b:v': '0'}),
-            Format('webm-vp9-nq', 'WEBM', 'Normal Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '40', '-b:v': '0'}),
-            Format('webm-vp9-hq', 'WEBM', 'High Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '30', '-b:v': '0'}),
-            Format('webm-vp9-uq', 'WEBM', 'Ultra Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '20', '-b:v': '0'}),
+            Format('webm-vp9-prevq', 'WebM', 'Draft Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '60', '-b:v': '0'}),
+            Format('webm-vp9-lq', 'WebM', 'Low Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '50', '-b:v': '0'}),
+            Format('webm-vp9-nq', 'WebM', 'Normal Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '40', '-b:v': '0'}),
+            Format('webm-vp9-hq', 'WebM', 'High Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '30', '-b:v': '0'}),
+            Format('webm-vp9-uq', 'WebM', 'Ultra Quality', 'webm', 'video/webm', 'VP9', 'libvpx-vp9', {'-crf': '20', '-b:v': '0'}),
+
+            Format('webm-vp8-lq', 'WebM', 'Low Quality', 'webm', 'video/webm', 'VP8', 'libvpx', {'-crf': '50', '-b:v': '0'}),
+            Format('webm-vp8-nq', 'WebM', 'Normal Quality', 'webm', 'video/webm', 'VP8', 'libvpx', {'-crf': '40', '-b:v': '0'}),
+            Format('webm-vp8-hq', 'WebM', 'High Quality', 'webm', 'video/webm', 'VP8', 'libvpx', {'-crf': '30', '-b:v': '0'}),
 
             Format('avi-mpeg4-lq', 'AVI', 'Low Quality', 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '22'}),
             Format('avi-mpeg4-nq', 'AVI', 'Normal Quality', 'avi', 'video/x-msvideo', 'MPEG-4', 'mpeg4', {'-qscale:v': '10'}),
