5672704061e179710b3250f7e9ebcfcc640e9c1f
Christoph Muche <christoph@muche.info>
2023-05-25 16:57:53 +0200

    format

--- octoprint_timelapseplus/static/js/timelapseplus.js
+++ octoprint_timelapseplus/static/js/timelapseplus.js
@@ -89,8 +89,8 @@ $(function() {
         });
 
         self.getRandomString = function(length = 16) {
-            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            let result = '';
+            const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            let result = "";
 
             for (let i = 0; i < length; i++) {
                 const randomIndex = Math.floor(Math.random() * characters.length);
