7e5b87747a6877bae0babd3127610b2e52a17caa
Christoph Muche <christoph@muche.info>
2023-05-28 16:28:11 +0200

    use relative z

--- octoprint_timelapseplus/helpers/stabilizationHelper.py
+++ octoprint_timelapseplus/helpers/stabilizationHelper.py
@@ -1,23 +1,36 @@
+from ..constants import Constants
+
+
 class StabilizationHelper:
 
     def __init__(self, settings):
+        self.SNAPSHOT_COMMAND = settings.get(["snapshotCommand"])
         self.RETRACT_SPEED = 30
         self.RETRACT_AMOUNT = 1.2
+        self.RETRACT_ZHOP = 0.2
         self.MOVE_SPEED = 150
         self.PARK_X = 30
         self.PARK_Y = 125
+        self.PARK_Z = 2
+        self.PARK_Z_RELATIVE = True
         self.WAIT_FOR_MOVEMENT = True
         self.WAIT_BEFORE = 200
         self.WAIT_AFTER = 100
 
     def stabilizeAndQueueSnapshotRaw(self, printer, positionTracker):
         if printer.set_job_on_hold(True):
+
+            shouldDoRetract = self.RETRACT_AMOUNT > 0 or self.RETRACT_ZHOP > 0
+            newZPos = self.PARK_Z
+            if self.PARK_Z_RELATIVE:
+                newZPos = positionTracker.POS_Z + self.PARK_Z
+
             cmd = []
             try:
-                if self.RETRACT_AMOUNT > 0:
-                    cmd.append('G1 G1 E-' + str(self.RETRACT_AMOUNT) + ' F' + str(self.RETRACT_SPEED * 60))
+                if shouldDoRetract:
+                    cmd.append('G1 E-' + str(self.RETRACT_AMOUNT) + ' Z' + str(self.RETRACT_ZHOP) + ' F' + str(self.RETRACT_SPEED * 60))
 
-                cmd.append('G0 X' + str(self.PARK_X) + ' Y' + str(self.PARK_Y) + ' F' + str(self.MOVE_SPEED * 60))
+                cmd.append('G0 X' + str(self.PARK_X) + ' Y' + str(self.PARK_Y) + ' Z' + str(newZPos) + ' F' + str(self.MOVE_SPEED * 60))
 
                 if self.WAIT_FOR_MOVEMENT:
                     cmd.append('M400')
@@ -25,16 +38,16 @@ class StabilizationHelper:
                 if self.WAIT_BEFORE > 0:
                     cmd.append('G4 P' + int(self.WAIT_BEFORE))
 
-                # todo snapshot command from settings
-                cmd.append('@SNAPSHOT-UNSTABLE')
+                # todo snapshot and suffix command from settings
+                cmd.append('@' + self.SNAPSHOT_COMMAND + '-' + Constants.SUFFIX_PRINT_UNSTABLE)
 
                 if self.WAIT_AFTER > 0:
                     cmd.append('G4 P' + int(self.WAIT_AFTER))
 
-                cmd.append('G0 X' + str(positionTracker.POS_X) + ' Y' + str(positionTracker.POS_Y) + ' F' + str(self.MOVE_SPEED * 60))
+                cmd.append('G0 X' + str(positionTracker.POS_X) + ' Y' + str(positionTracker.POS_Y) + ' Z' + str(positionTracker.POS_Z) + ' F' + str(self.MOVE_SPEED * 60))
 
-                if self.RETRACT_AMOUNT > 0:
-                    cmd.append('G1 G1 E' + str(self.RETRACT_AMOUNT) + ' F' + str(self.RETRACT_SPEED * 60))
+                if shouldDoRetract:
+                    cmd.append('G1 E' + str(self.RETRACT_AMOUNT) + ' Z-' + str(self.RETRACT_ZHOP) + ' F' + str(self.RETRACT_SPEED * 60))
 
                 printer.commands(cmd)
             finally:
