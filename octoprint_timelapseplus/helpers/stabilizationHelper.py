32c556fddf02c84d3466f6880ddfd639ee122541
Christoph Muche <christoph@muche.info>
2023-06-06 20:54:12 +0200

    refactor, modify abs/rel mode

--- octoprint_timelapseplus/helpers/stabilizationHelper.py
+++ octoprint_timelapseplus/helpers/stabilizationHelper.py
@@ -7,20 +7,54 @@ class StabilizationHelper:
         self.SNAPSHOT_COMMAND = settings.get(["snapshotCommand"])
         self.STAB = stabilizationSettings
 
+    def getRetractionCommands(self, positionTracker, inverse):
+        shouldDoRetract = self.STAB.RETRACT_AMOUNT > 0 or self.STAB.RETRACT_Z_HOP > 0
+        if not shouldDoRetract:
+            return []
+
+        cmd = []
+
+        if not positionTracker.RELATIVE_MODE:
+            cmd.append('G91')
+        if not positionTracker.RELATIVE_MODE_EXTRUDER:
+            cmd.append('M83')
+
+        if inverse:
+            cmd.append('G1 E' + str(self.STAB.RETRACT_AMOUNT) + ' Z-' + str(self.STAB.RETRACT_Z_HOP) + ' F' + str(self.STAB.getFeedrateRetraction()))
+        else:
+            cmd.append('G1 E-' + str(self.STAB.RETRACT_AMOUNT) + ' Z' + str(self.STAB.RETRACT_Z_HOP) + ' F' + str(self.STAB.getFeedrateRetraction()))
+
+        if not positionTracker.RELATIVE_MODE:
+            cmd.append('G90')
+        if not positionTracker.RELATIVE_MODE_EXTRUDER:
+            cmd.append('M82')
+
+        return cmd
+
+    def getMoveCommands(self, positionTracker, x, y, z, f):
+        cmd = []
+
+        if not positionTracker.RELATIVE_MODE:
+            cmd.append('G91')
+
+        if not positionTracker.RELATIVE_MODE:
+            cmd.append('G90')
+
+        cmd.append('G0 X' + str(x) + ' Y' + str(y) + ' Z' + str(z) + ' F' + str(f))
+
+        return cmd
+
     def stabilizeAndQueueSnapshotRaw(self, printer, positionTracker):
         if printer.set_job_on_hold(True):
 
-            shouldDoRetract = self.STAB.RETRACT_AMOUNT > 0 or self.STAB.RETRACT_Z_HOP > 0
             newZPos = self.STAB.PARK_Z
             if self.STAB.PARK_Z_RELATIVE:
                 newZPos = positionTracker.POS_Z + self.STAB.PARK_Z
 
             cmd = []
             try:
-                if shouldDoRetract:
-                    cmd.append('G1 E-' + str(self.STAB.RETRACT_AMOUNT) + ' Z' + str(self.STAB.RETRACT_Z_HOP) + ' F' + str(self.STAB.RETRACT_SPEED * 60))
-
-                cmd.append('G0 X' + str(self.STAB.PARK_X) + ' Y' + str(self.STAB.PARK_Y) + ' Z' + str(newZPos) + ' F' + str(self.STAB.MOVE_SPEED * 60))
+                cmd += self.getRetractionCommands(positionTracker, False)
+                cmd += self.getMoveCommands(positionTracker, self.STAB.PARK_X, self.STAB.PARK_Y, newZPos, self.STAB.getFeedrateMove())
 
                 if self.STAB.WAIT_FOR_MOVEMENT:
                     cmd.append('M400')
@@ -33,10 +67,8 @@ class StabilizationHelper:
                 if self.STAB.WAIT_AFTER > 0:
                     cmd.append('G4 P' + str(self.STAB.WAIT_AFTER))
 
-                cmd.append('G0 X' + str(positionTracker.POS_X) + ' Y' + str(positionTracker.POS_Y) + ' Z' + str(positionTracker.POS_Z) + ' F' + str(self.STAB.MOVE_SPEED * 60))
-
-                if shouldDoRetract:
-                    cmd.append('G1 E' + str(self.STAB.RETRACT_AMOUNT) + ' Z-' + str(self.STAB.RETRACT_Z_HOP) + ' F' + str(self.STAB.RETRACT_SPEED * 60))
+                cmd += self.getMoveCommands(positionTracker, positionTracker.POS_X, positionTracker.POS_Y, positionTracker.POS_Z, self.STAB.getFeedrateMove())
+                cmd += self.getRetractionCommands(positionTracker, True)
 
                 printer.commands(cmd, force=True)
             finally:
